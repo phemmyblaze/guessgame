6bd5c9c93c3d029e9489d1349a432f1f96bf15cd
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-02-13 18:19:50 +0100

    Code base refactoring

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ const secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
 
 
 document.querySelector('.check').addEventListener('click', function(e) {
     const guess = document.querySelector('.guess').value;
 
     if(!guess) {
-        document.querySelector('.message').textContent = 'Please enter a number';
+        // document.querySelector('.message').textContent = 'Please enter a number';
+        displayMessage('Please enter a number');
 
         ////TESTING FOR CORRECT INPUT
 
     } else if (guess == secretNumber ) {
-        document.querySelector('.message').textContent = 'Correct!';
+        // document.querySelector('.message').textContent = 'Correct!';
+        displayMessage('Correct!');
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
@@ -27,36 +33,23 @@ document.querySelector('.check').addEventListener('click', function(e) {
         
       }
         
-        ////TESTING IF THE NUMBER IS TOO HIGH
+        ////TESTING IF THE GUESS IS WRONG
 
-    } else if (guess > secretNumber) { 
-
+    } else if (guess !== secretNumber) {
         if(score > 1) {
-        document.querySelector('.message').textContent = 'Too high!';
-        document.querySelector('body').style.backgroundColor = 'purple';
-        score--;
-        document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game';
-            document.querySelector('.score').textContent = 0;
-            document.querySelector('body').style.backgroundColor = 'red';
-        }
-
-        ///TESTING IF THE NUMBER ENTERED IS TOO LOW
-        
-    } else if (guess < secretNumber) {
-
-        if(score > 1) {
-            document.querySelector('.message').textContent = 'Too low!';
-            document.querySelector('body').style.backgroundColor = 'blue';
+            // document.querySelector('.message').textContent = guess > secretNumber ?'Too high!' : "Too Low";
+            displayMessage(guess > secretNumber ?'Too high!' : "Too Low");
+            document.querySelector('body').style.backgroundColor = 'purple';
             score--;
             document.querySelector('.score').textContent = score;
             } else {
-                document.querySelector('.message').textContent = 'You lost the game';
+                displayMessage('Game Over!');
                 document.querySelector('.score').textContent = 0;
                 document.querySelector('body').style.backgroundColor = 'red';
             }
     }
+
+    
 })
 
 
@@ -65,7 +58,7 @@ document.querySelector('.check').addEventListener('click', function(e) {
 
 document.querySelector('.again').addEventListener('click', function(e){
     score = 20;
-    document.querySelector('.message').textContent = 'Start guessing....';
+    displayMessage('Guess....')
     document.querySelector('.number').textContent = '?';
     document.querySelector('.score').textContent = score;
     document.querySelector('.guess').textContent = '';
@@ -76,53 +69,17 @@ document.querySelector('.again').addEventListener('click', function(e){
 
 
 
-/*
-class profile {
-    constructor(name, age, gender) {    
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
 
-        // return (name, age, gender);
-
-    }
-
-    getProfile = function() {
-        return `My name is ${this.name} and am I ${this.age} years old, am a ${this.gender}`
-    }
-}
-
-let profile2 = new profile('John', 20, 'male');
-console.log(profile2.getProfile());
-
-let profile3 = new profile('Jenifer', 20, 'female');
-console.log(profile3.getProfile());
-*/
-
-// class person {
-//     constructor(name, birthYear, gender) {
-//         this.name = name;
-//         this.birthYear = birthYear;
-//         this.gender = gender;
-//     }
-//     calcAge(){
-//         console.log(new Date().getFullYear()-this.birthYear);
-//     }
-// }
-
-// person.prototype.greet = function(){
-//     console.log(`Good afternoon ${this.name}`)
-// }
-// let john = new person('john', 1997, 'male');
-// console.log(john)
-// john.greet();
-// john.calcAge();
 
 
 class B{
     constructor(age) {
         this.age = age;
+
+        console.log(this.age)
     }
+    
+    
 }
 
 class Test extends B {
